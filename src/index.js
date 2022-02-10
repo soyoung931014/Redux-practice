e1e7a284be00093a596561a89d0155b133ad934d
park_so_young <soyoung931014@gmail.com>
2022-02-10 22:19:30 +0900

    Subscripitions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 //** action은 redux에서 function을 부를 때 쓰는 두 번쨰 parameter, 혹은 argument이다. */
+//**  dispatch는 action에 보내준다.
+//**  subscribe는 store안에 있는 데이터 변화들을 알 수 있게 해준다. countStore.subscribe(f)
 import { createStore } from "redux"
 
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
-let count = 0
-const countModifier = (count= 0 , action) => {
+
+const countModifier = (count = 0 , action) => {
   console.log(count, action) //*{type: '@@redux/INIT9.t.3.3.c'} // {type: 'hello'}
   if (action.type === 'ADD') {
     return count + 1
@@ -16,16 +18,17 @@ const countModifier = (count= 0 , action) => {
   return count
 }
 
-
+const handleMinus = () => {
+  countStore.dispatch({type: "Minus"})
+}
 
 const countStore = createStore(countModifier) // store 생성
 
-// console.log(countStore) {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
+const onChange = () => {
+  //console.log("there was a change on the store") // 이렇게 하고 버튼을 계속 누르면 store에 변화가 있다.  
+  number.innerText = countStore.getState()
+}
+countStore.subscribe(onChange)
 
-
-//* dispatch라는 함수를 이용해 action을 할 수 있다. // 아래 처럼 작성을 한 후 console을 찍으면{type: 'hello'}
-countStore.dispatch({type:'ADD'})
-countStore.dispatch({type:'ADD'})
-countStore.dispatch({type:'ADD'})
-countStore.dispatch({type:'Minus'})
-countStore.getState(count)
+add.addEventListener("click", () => countStore.dispatch({type:"ADD"}))
+minus.addEventListener("click", handleMinus)
