13a119a73562312763b4cfa189a0a2ad64c9a2fa
park_so_young <soyoung931014@gmail.com>
2022-02-11 00:18:53 +0900

    2.0 TODOLIST: Vanilla TODO

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
-//** action은 redux에서 function을 부를 때 쓰는 두 번쨰 parameter, 혹은 argument이다. */
-//**  dispatch는 action에 보내준다.
-//**  subscribe는 store안에 있는 데이터 변화들을 알 수 있게 해준다. countStore.subscribe(f)
-//** 만약 녜가 change를 너의 store에서 감지하고 싶다면 change를 구독하면된다.(countStore.subscribe(f)) */
-// 개선사항 1. switch case쓰기 2. type:"" string쓰면 오타날 수 있으니까 오타 방지 위해  변수 선언해주기
+//* 바닐라자바스크립트 + 리덕스 이용하기
+//* MUTATE STATE 절대 사용하지 말기
+import   {createStore} from "redux"
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
 
-import { createStore } from "redux"
 
-const add = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
+// const createlist = (content) => {
+//   const li = document.createElement("li")
+//   li.innerText = content;
+//   ul.appendChild(li)
+// }
 
-number.innerText = 0
+const onSubmit = (e) => {
+  e.preventDefault()
+  const toDo = input.value
+  input.value = ""
+  store.dispatch({type: ADD_TODO, text: toDo})
+}
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
 
-const ADD ="ADD"
-const MINUS = "MINUS"
-const countModifier = (count = 0 , action) => {
- switch (action.type) {
-  case ADD:
-    return count + 1
-  case MINUS:
-    return count -1
-  default:
-    return count  
+const reducer = (currentState =[], action) => {
+  console.log(action) // {type: 'ADD_TODO', text: 'hi'}
+  switch (action.type) {
+    case ADD_TODO:
+      return []
+    case DELETE_TODO:
+      return []  
+    default:
+      return []  
   }
-  }
-
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS})
 }
 
-const countStore = createStore(countModifier) // store 생성
+const store = createStore(reducer)
 
-const onChange = () => {
-  //console.log("there was a change on the store") // 이렇게 하고 버튼을 계속 누르면 store에 변화가 있다.  
-  number.innerText = countStore.getState()
-}
-countStore.subscribe(onChange)
-
-add.addEventListener("click", () => countStore.dispatch({type:ADD}))
-minus.addEventListener("click", handleMinus)
+form.addEventListener("submit", onSubmit)
