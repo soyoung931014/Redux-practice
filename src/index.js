4679470fe7defcf6d58caca6a5e445cadd3acf56
park_so_young <soyoung931014@gmail.com>
2022-02-10 17:55:29 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-//** state란? 애플리케이션에서 변하는 데이터
-// ** 리덕스는 data(관리하는데)를 도와주는 역할을 하기 위해 만들어짐. 그 데이터는 store에 저장됨
+//** action은 redux에서 function을 부를 때 쓰는 두 번쨰 parameter, 혹은 argument이다. */
 import { createStore } from "redux"
+
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
-const countModifier = (state) => {
-  // 인자 state = 0으로 하면 0 이나온다.
-  //return 'hello' // 여기 리턴하는것이 곧 데이터
-  console.log(state) // undefined
-  return state
+let count = 0
+const countModifier = (count= 0 , action) => {
+  console.log(count, action) //*{type: '@@redux/INIT9.t.3.3.c'} // {type: 'hello'}
+  if (action.type === 'ADD') {
+    return count + 1
+  } else if (action.type === 'Minus') {
+    return count - 1
+  }
+  return count
 }
-const countStore = createStore(countModifier) // createStore의 인자에는 함수 reducer이 필요
-console.log(countStore) 
-//**{dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}  */
 
-console.log(countStore.getState()) // hello
 
-// <기억할것>
-//** */ 1. 처음 store(data를 저장하는곳)을 만든다.
-//** */ 2. store를 만들면 reducer라는것을 만들어주는데 이것은 데이터를 수정해주는 함수(function)이다.
-//** */ 3. store의 데이터를 유일하게 수정해 줄 수 있는 것은 reducer함수 뿐이다.
+
+const countStore = createStore(countModifier) // store 생성
+
+// console.log(countStore) {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
+
+
+//* dispatch라는 함수를 이용해 action을 할 수 있다. // 아래 처럼 작성을 한 후 console을 찍으면{type: 'hello'}
+countStore.dispatch({type:'ADD'})
+countStore.dispatch({type:'ADD'})
+countStore.dispatch({type:'ADD'})
+countStore.dispatch({type:'Minus'})
+countStore.getState(count)
